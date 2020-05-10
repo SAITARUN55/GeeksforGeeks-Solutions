f8f5f6ee64017f6590db109987161ea5fffefd81
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-10 18:51:03 +0530

    Create Addition of Two Numbers.cpp

---
+++ Mathematical/Addition of Two Numbers/Addition of Two Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,sum;
+	    cin>>a>>b;
+	    sum=a+b;
+	    cout<<sum<<"\n";
+	}
+	return 0;
+}
