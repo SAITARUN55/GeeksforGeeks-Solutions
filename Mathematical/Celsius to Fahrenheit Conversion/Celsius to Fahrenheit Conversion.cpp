1c6680daf9162c24c011c8b62447df6c311180cd
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-10 23:20:20 +0530

    Create Celsius to Fahrenheit Conversion.cpp

---
+++ Mathematical/Celsius to Fahrenheit Conversion/Celsius to Fahrenheit Conversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int c,f;
+	    cin>>c;
+	    f=((c*9)/5)+32;
+	    cout<<f<<"\n";
+	}
+	return 0;
+}
