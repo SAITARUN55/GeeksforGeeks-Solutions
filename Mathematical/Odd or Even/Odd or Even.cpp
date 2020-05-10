7b8e645c3014252a9d09aeb4d2bae0265775b849
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-10 22:19:04 +0530

    Create Odd or Even.cpp

---
+++ Mathematical/Odd or Even/Odd or Even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%2==0){
+	        cout<<"even"<<"\n";
+	    }
+	    else{
+	        cout<<"odd"<<"\n";
+	    }
+	}
+	return 0;
+}
