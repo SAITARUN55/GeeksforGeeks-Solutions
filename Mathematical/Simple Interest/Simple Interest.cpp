42c017159f49b9a0d17bc2db5f3ea5c94f0ccc4e
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-10 18:33:12 +0530

    Create Simple Interest.cpp

---
+++ Mathematical/Simple Interest/Simple Interest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int p,t,r,si;
+	    cin>>p>>t>>r;
+	    si = (p*t*r)/100;
+	    cout<<si<<"\n";
+	}
+	return 0;
+}
