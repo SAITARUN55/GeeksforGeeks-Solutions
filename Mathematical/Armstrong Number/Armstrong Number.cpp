1dd35834a2069613350dae2c0e81c43155173bda
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-11 22:28:30 +0530

    Create Armstrong Number.cpp

---
+++ Mathematical/Armstrong Number/Armstrong Number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,result,temp,rem;
+	    cin>>n;
+	    temp=n;
+	    while(temp!=0){
+	        rem=n%10;
+	        result +=rem*rem*rem;
+	        temp=temp/10;
+	    }
+	    if(result==n){
+	        cout<<"Yes"<<"\n";
+	    }
+	    else{
+	        cout<<"No"<<"\n";
+	    }
+	    }
+	return 0;
+}
