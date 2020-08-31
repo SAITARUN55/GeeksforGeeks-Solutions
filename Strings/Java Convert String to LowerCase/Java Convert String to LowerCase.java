7da70d6ee7ce48cf7c4e0db46d6bea6ec8286bff
Sai Tarun Kaniganti <saitarunkaniganti@gmail.com>
2020-08-31 23:19:25 +0530

    Create Java Convert String to LowerCase.java

---
+++ Strings/Java Convert String to LowerCase/Java Convert String to LowerCase.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+public static void main (String[] args) {
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+sc.nextLine();
+while(t-->0)
+{
+String s=sc.nextLine();
+s=s.toLowerCase();
+System.out.println(s);
+}
+}
+}
